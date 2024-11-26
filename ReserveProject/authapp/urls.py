8b547e66fd8f8598d4ce397109aff12e5f145321
HEIL-TCHAMBA-NANA <htchamba124@gmail.co>
2024-11-26 17:35:33 +0100

    full authentication

---
+++ ReserveProject/authapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('signup/', RegisterView.as_view(), name='signup'),  # Ajoutez .as_view()
+    path('approve-delegate/<int:user_id>/', ApproveDelegateView.as_view(), name='approve-delegate'),  # Ajoutez .as_view()
+    path('login/', LoginView.as_view(), name='login'),
+]
