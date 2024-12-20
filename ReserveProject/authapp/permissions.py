2a65feca6c29444fb08c38c400d0aa0b47c718e9
HEIL-TCHAMBA-NANA <htchamba124@gmail.co>
2024-12-20 17:19:19 +0100

    Final commit

--- ReserveProject/authapp/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-from .models import CustomUser
-
-class IsAdministrator(BasePermission):
-    """
-    Permission pour vérifier si l'utilisateur authentifié est un administrateur.
-    """
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role == 'administrateur'
