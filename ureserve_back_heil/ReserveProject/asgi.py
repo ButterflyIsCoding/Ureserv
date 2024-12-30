296637e9b5d88994edb4afc051d10d119e8f94a9
heil <heil@localhost.localdomain>
2024-12-30 17:12:24 +0100

    new final commit deploy

--- ureserve_back_heil/ReserveProject/asgi.py
+++ ureserve_back_heil/ReserveProject/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -13,21 +12,3 @@ application = ProtocolTypeRouter({
         URLRouter(websocket_urlpatterns)
     ),
 })
-=======
-"""
-ASGI config for ReserveProject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReserveProject.settings')
-
-application = get_asgi_application()
->>>>>>> 346bbacefa667e29d78f773503ee09874ed8cf05
