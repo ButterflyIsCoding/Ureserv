f405746993688e1bc637610060f41a5e31518deb
heil <heil@localhost.localdomain>
2024-12-30 10:13:49 +0100

    Commit LuNa

--- ReserveProject/reservationapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Hall, Reservation, Notification
-
-admin.site.register(Hall)
-admin.site.register(Reservation)
-admin.site.register(Notification)
